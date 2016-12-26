68b856c889c51fc6c774198f506344fb9aa46f91
wchen <sdjcw13@gmail.com>
2016-12-26 16:48:06 +0800

    完善抢红包示例

--- routes/money-package.js
+++ routes/money-package.js
@@ -1,6 +1,6 @@
 var router = require('express').Router();
 var _ = require('underscore');
-var Q = require('q');
+var bluebird = require('bluebird');
 var debug = require('debug')('app');
 
 var AV = require('leanengine');
@@ -47,7 +47,7 @@ router.post('/generate', function(req, res, next) {
 router.post('/:id/ready', function(req, res, next) {
   var query = new AV.Query(MoneyPackage);
   query.get(req.params.id).then(function(moneyPackage) {
-    return Q.ninvoke(redisConn, 'hset', 'moneyPackages', req.params.id, JSON.stringify({
+    return redisConn.hsetAsync('moneyPackages', req.params.id, JSON.stringify({
       id: moneyPackage.id,
       total: moneyPackage.get('total'),
       count: moneyPackage.get('count'),
@@ -59,14 +59,12 @@ router.post('/:id/ready', function(req, res, next) {
     return moneyPackage.save();
   }).then(function() {
     res.redirect('/money-package');
-  }).catch(function(err) {
-    next(err);
-  });
+  }).catch(next);
 });
 
 router.get('/:id/rush', function(req, res, next) {
   AV.Promise.when([
-    Q.ninvoke(redisConn, 'hget', 'moneyPackages', req.params.id),
+    redisConn.hgetAsync('moneyPackages', req.params.id),
     new AV.Query(User).find() // 查询 User 只是为了再抢红包时模拟用户身份
   ]).then(function(moneyPackage, users) {
     res.render('money-package/rush', {
@@ -79,23 +77,56 @@ router.get('/:id/rush', function(req, res, next) {
 router.post('/:id/rush', function(req, res, next) {
   var moneyPackage;
   var multi = redisConn.multi();
+  var userId = req.body.user;
   multi.hget('moneyPackages', req.params.id);
   multi.llen('moneyPackages:' + req.params.id);
-  Q.ninvoke(multi, 'exec').then(function(datas) {
-    moneyPackage = JSON.datas[0];
+  multi.execAsync().then(function(datas) {
+    moneyPackage = JSON.parse(datas[0]);
     if (moneyPackage.count < datas[1]) { // 如果红包数量小于抢红包队列长度，则说明红包已经抢完
-      throw new Error('红包已抢完');
+      var err = new Error('红包已抢完');
+      err.status = 400;
+      throw err;
     }
-    return Q.ninvoke(redisConn, 'rpush', 'moneyPackages:' + req.params.id);
+    return redisConn.rpushAsync('moneyPackages:' + req.params.id, userId);
   }).then(function(newLength) {
     // 即使插入队列成功也不代表抢到红包，因为在上面查询队列长度到添加队列期间可能队列已经被别的请求增加了，
     // 所以根据 Redis 返回的长度判断抢红包是否有效。
     if (moneyPackage.count < newLength) {
-      throw new Error('红包已抢完');
+      var err = new Error('红包已抢完');
+      err.status = 400;
+      throw err;
     }
     res.send('恭喜，抢到红包 ' + moneyPackage.packages[newLength - 1] + ' 元');
-  });
+  }).catch(next);
 });
+
+router.post('/:id/done', function(req, res, next) {
+  var id = req.params.id;
+  bluebird.all([
+    new AV.Query(MoneyPackage).get(id),
+    redisConn.lrangeAsync('moneyPackages:' + id, 0, -1)
+  ]).spread(function(moneyPackage, users) {
+    users = users.map(function(id) {
+      return AV.Object.createWithoutData('_User', id);
+    })
+    moneyPackage.set('users', users.slice(0, moneyPackage.get('count')));
+    moneyPackage.set('status', 'done');
+    return moneyPackage.save();
+  }).then(function() {
+    return redisConn.delAsync('moneyPackages:' + id);
+  }).then(function() {
+    res.redirect('/money-package');
+  }).catch(next);
+})
+
+router.get('/:id/result', function(req, res, next) {
+  var id = req.params.id;
+  new AV.Query(MoneyPackage).include('users').get(id)
+  .then(function(moneyPackage) {
+    res.send(moneyPackage);
+  })
+  .catch(next);
+})
 
 var generateMoneyPackages = function(total, count) {
   var result = [];
